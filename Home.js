b73c94e53b62831de14dde8d69c9d013a9657cb5
Bruno Avelino <brunoac.cc@gmail.com>
2017-03-09 23:42:09 -0300

    Home: centralizado as funções para o controle do inc/dec/display

--- Home.js
+++ Home.js
@@ -1,52 +1,35 @@
 const Home = function(domQtdSeries, domDuracaoSerie, domDuracaoIntervalo) {
-	const self = this;
-	const $qtdSeries = $(domQtdSeries), $duracaoSerie = $(domDuracaoSerie), $duracaoIntervalo = $(domDuracaoIntervalo);
+	const layoutQtdSeries = {
+		valor: 10,
+		$ : $(domQtdSeries),
+		step : 1,
+		max : 99,
+		min : 1
+	};
+	const layoutDuracaoSerie = {
+		valor: 25,
+		$ : $(domDuracaoSerie),
+		step : 5,
+		max : 95,
+		min : 5
+	};
+	const layoutDuracaoIntervalo = {
+		valor: 15,
+		$ : $(domDuracaoIntervalo),
+		step : 5,
+		max : 95,
+		min : 5
+	};
 
-	let qtdSeries = 10;
-	const changeQtdSeries = function(novoValor) {
-		qtdSeries = novoValor;
-		digitalDisplayText($qtdSeries.find('.digital-display'), qtdSeries);
-	}
-	const incQtdSeries = function() {
-		changeQtdSeries(qtdSeries = Math.min(qtdSeries+1, 99));
-	}
-	const decQtdSeries = function() {
-		changeQtdSeries(qtdSeries = Math.max(qtdSeries-1, 1));
-	}
+	initDisplay(layoutQtdSeries);
+	initDisplay(layoutDuracaoSerie);
+	initDisplay(layoutDuracaoIntervalo);
 
-	$qtdSeries.find('.btn-inc').click(incQtdSeries);
-	$qtdSeries.find('.btn-dec').click(decQtdSeries);
-	digitalDisplayText($qtdSeries.find('.digital-display'), qtdSeries);
-
-	let duracaoSerie = 30;
-	const changeDuracaoSerie = function(novoValor) {
-		duracaoSerie = novoValor;
-		digitalDisplayText($duracaoSerie.find('.digital-display'), duracaoSerie);
+	this.getValores = function() {
+		return {
+			qtdSeries: layoutQtdSeries.valor,
+			duracaoSerie: layoutDuracaoSerie.valor,
+			duracaoIntervalo: layoutDuracaoIntervalo.valor
+		};
 	}
-	const incDuracaoSerie = function() {
-		changeDuracaoSerie(duracaoSerie = Math.min(duracaoSerie+5, 95));
-	}
-	const decDuracaoSerie = function() {
-		changeDuracaoSerie(duracaoSerie = Math.max(duracaoSerie-5, 5));
-	}
-
-	$duracaoSerie.find('.btn-inc').click(incDuracaoSerie);
-	$duracaoSerie.find('.btn-dec').click(decDuracaoSerie);
-	digitalDisplayText($duracaoSerie.find('.digital-display'), duracaoSerie);
-
-	let duracaoIntervalo = 15;
-	const changeDuracaoIntervalo = function(novoValor) {
-		duracaoIntervalo = novoValor;
-		digitalDisplayText($duracaoIntervalo.find('.digital-display'), duracaoIntervalo);
-	}
-	const incDuracaoIntervalo = function() {
-		changeDuracaoIntervalo(duracaoIntervalo = Math.min(duracaoIntervalo+5, 95));
-	}
-	const decDuracaoIntervalo = function() {
-		changeDuracaoIntervalo(duracaoIntervalo = Math.max(duracaoIntervalo-5, 5));
-	}
-
-	$duracaoIntervalo.find('.btn-inc').click(incDuracaoIntervalo);
-	$duracaoIntervalo.find('.btn-dec').click(decDuracaoIntervalo);
-	digitalDisplayText($duracaoIntervalo.find('.digital-display'), duracaoIntervalo);
 }
--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const digitalDisplayText = ($d, v) => $d.text(('00'+v).slice(-2));
-
-$('#home').show();
-$('#options').hide();
-$('#running').hide();
+const initDisplay = (layout) => {
+	const change = function(novoValor) {
+		layout.valor = novoValor;
+		digitalDisplayText(layout.$.find('.digital-display'), layout.valor);
+	}
+	layout.$.find('.btn-inc').click(() => {
+		change(Math.min(layout.valor+layout.step, layout.max));
+	});
+	layout.$.find('.btn-dec').click(() => {
+		change(Math.max(layout.valor-layout.step, layout.min));
+	});
+	digitalDisplayText(layout.$.find('.digital-display'), layout.valor);
+}
 
 
 jQuery(document).ready(function($) {
@@ -13,9 +22,13 @@ jQuery(document).ready(function($) {
 	$('<div>').html((html_serie + html_intervalo).replace(/\$\{index\}/g, 2)).appendTo('#options .content-option');
 	$('<div>').html((html_serie).replace(/\$\{index\}/g, 3)).appendTo('#options .content-option');
 	
-	new Home(
-		$(home).find('.qtd-series'),
-		$(home).find('.duracao-series'),
-		$(home).find('.duracao-intervalo')
+	const home = new Home(
+		$('#home').find('.qtd-series'),
+		$('#home').find('.duracao-series'),
+		$('#home').find('.duracao-intervalo')
 	);
+
+	$('#home').show();
+	$('#options').hide();
+	$('#running').hide();
 });
